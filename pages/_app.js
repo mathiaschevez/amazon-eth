a6620475e4b6d33fd57698fa1ed22f7c04b3805d
Mathias Chevez <solo@Mathiass-MacBook-Air.local>
2022-05-13 15:16:56 -0600

    authenticate crypto wallet and set username

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
+import { ContextProvider } from '../context/Context'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}
+      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+    >
+      <ContextProvider>
+        <Component {...pageProps} />
+      </ContextProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
